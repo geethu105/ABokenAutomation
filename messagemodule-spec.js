d36bf6552cc46e9992aa2dbec07ec85a817250bf
Geetha <geethasaravanan@asteroidsoft.com>
2018-11-09 17:59:55 +0530

    message_module_menu_check

---
+++ messagemodule-spec.js
@@ -0,0 +1,11 @@
+describe('Verify Company Admin users page and Header Menus', function() {
+    browser.waitForAngularEnabled(false);
+    {
+    // Verify the message module submenus.   
+    it('Verify the message menu', function() {
+    //Verify the message menu Available in the page
+               var Messagemenu = element(by.linkText('Message'));
+                expect(Messagemenu.isDisplayed()).toBeTruthy(); 
+                });
+    }
+});
