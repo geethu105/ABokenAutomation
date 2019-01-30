8a24818e28cc35e2bcc8cdcf36323835ef525da2
Geetha <geethasaravanan@asteroidsoft.com>
2019-01-30 11:15:20 +0530

    just changes

--- messagemodule-spec.js
+++ messagemodule-spec.js
@@ -2,7 +2,7 @@ describe('Verify Company Admin users page and Header Menus', function() {
     browser.waitForAngularEnabled(false);
     {
     // Verify the message module submenus.   
-    it('Verify the message menu', function() {
+    it('Click on the message menu', function() {
     //Verify the message menu Available in the page
                var Messagemenu = element(by.linkText('Message'));
                 expect(Messagemenu.isDisplayed()).toBeTruthy(); 
